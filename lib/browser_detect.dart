6cdce8cd7a097ac38bd50eb19168cf44481fd370
Dan Schultz <schultz.t.dan@gmail.com>
2013-09-20 02:07:29 -0700

    Check if navigator.vendor is null
    IE doesn't have this field

--- lib/browser_detect.dart
+++ lib/browser_detect.dart
@@ -18,15 +18,15 @@ Browser get browser {
 Iterable<Browser> _browsers = [_chrome, _safari, _opera, _ie, _firefox];
 
 Browser _chrome = new Browser("Chrome",
-    () => window.navigator.vendor.contains("Google"),
+    () => _matchVendor("Google"),
     () => new RegExp(r"Chrome/(.*)\s").firstMatch(window.navigator.appVersion).group(1));
 
 Browser _safari = new Browser("Safari",
-    () => window.navigator.vendor.contains("Apple"),
+    () => _matchVendor("Apple"),
     () => new RegExp(r"Version/(.*)\s").firstMatch(window.navigator.appVersion).group(1));
 
 Browser _opera = new Browser("Opera",
-    () => window.navigator.vendor.contains("Opera"),
+    () => _matchVendor("Opera"),
     () => new RegExp(r"OPR/(.*)\s").firstMatch(window.navigator.appVersion).group(1));
 
 Browser _ie = new Browser("IE",
@@ -36,3 +36,8 @@ Browser _ie = new Browser("IE",
 Browser _firefox = new Browser("Firefox",
     () => window.navigator.userAgent.contains("Firefox"),
     () => new RegExp(r"rv:/(.*)\)").firstMatch(window.navigator.userAgent).group(1));
+
+bool _matchVendor(String name) {
+  var vendor = window.navigator.vendor;
+  return vendor != null && vendor.contains(name);
+}
